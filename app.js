9a5f89d579894dae1545c0ddd723463d8ea59d7a
GeekyNess <86745883+GeekyNess@users.noreply.github.com>
2022-01-28 22:20:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 
-
+var baseUrl = "https://api.coinranking.com/v2/coins";
+var proxyUrl = "https://cors-anywhere.herokuapp.com/"; 
 var apiKey = "coinrankingc178b86198a02aa036d1dcdd9f149338e6e3d5de4286c796"
